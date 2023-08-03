5ea3fd9244742b87836f0a4de4b7014a5f9063ab
Raphael  Araujo <raphael.araujo@accenture.com>
2023-08-03 01:56:52 +0100

    Criando nossa moldura, tela e o pincel.

--- src/main/java/br/com/mvbos/lgj/Janela.java
+++ src/main/java/br/com/mvbos/lgj/Janela.java
@@ -1,16 +1,40 @@
 package br.com.mvbos.lgj;
 
-import javax.swing.JFrame;
+import java.awt.Graphics;
 
-public class Janela extends JFrame{
-	
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Janela extends JFrame {
+
+	private JPanel tela;
+
 	public Janela() {
+
+		tela = new JPanel() {
+
+			/**
+			 * Nesse contexto o Jframe seria nossa moldura
+			 * Jpainel seria nossa tela
+			 * Aqui ciramos uma classe extendendo o Jpanel sobrescrevendo alguns metodos para termos mais flexibilidde
+			 * Pense na classe Graphics como sendo o nosso pincel na tela
+			 */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void paintComponent(Graphics g) {
+
+			}
+
+		};
+		
+		super.getContentPane().add(tela)
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(640, 480);
 		setVisible(true);
 	}
-	
-	
+
 	public static void main(String[] args) {
 		new Janela();
 	}
