7ae66efa1968acd1a463c89df5ab490fa99cdc2f
Raphael  Araujo <raphael.araujo@accenture.com>
2023-08-03 01:18:47 +0100

    Inicio do Projeto

---
+++ src/main/java/br/com/mvbos/lgj/Janela.java
@@ -0,0 +1,17 @@
+package br.com.mvbos.lgj;
+
+import javax.swing.JFrame;
+
+public class Janela extends JFrame{
+	
+	public Janela() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(640, 480);
+		setVisible(true);
+	}
+	
+	
+	public static void main(String[] args) {
+		new Janela();
+	}
+}
